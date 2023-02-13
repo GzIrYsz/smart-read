28833cf667bdf3ab2ce6fe48507ca811aa8317fb
Thomas REMY <contact.t.remy777@gmail.com>
2023-02-13 11:23:25 +0100

    OCR-31 feat: add custom exception
    add custom exception for unsupported file types and update current exception for unsupported word

--- spellchecking/src/main/java/fr/cyu/smartread/spellchecking/dictionary/WordNotSupportedException.java
+++ spellchecking/src/main/java/fr/cyu/smartread/spellchecking/dictionary/WordNotSupportedException.java
@@ -4,15 +4,29 @@ public class WordNotSupportedException extends IllegalArgumentException {
     private String word;
 
     public WordNotSupportedException(String word) {
-        this.word = word;
+        setWord(word);
     }
 
     public WordNotSupportedException(String word, String message) {
         super(message);
-        this.word = word;
+        setWord(word);
+    }
+
+    public WordNotSupportedException(String word, Throwable cause) {
+        super(cause);
+        setWord(word);
+    }
+
+    public WordNotSupportedException(String word, String message, Throwable cause) {
+        super(message, cause);
+        setWord(word);
     }
 
     public String getWord() {
         return this.word;
+    }
+
+    private void setWord(String word) {
+        this.word = word;
     }
 }
