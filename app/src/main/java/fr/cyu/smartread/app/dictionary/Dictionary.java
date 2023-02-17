516887637f39a31ea4ae878a10ef26d2cf422366
Thomas REMY <contact.t.remy777@gmail.com>
2023-02-17 22:45:48 +0100

    fix: remove useless class

--- app/src/main/java/fr/cyu/smartread/app/dictionary/Dictionary.java
+++
@@ -1,7 +0,0 @@
-package fr.cyu.smartread.app.dictionary;
-
-import java.util.Collection;
-
-public interface Dictionary {
-    Collection<String> getAssociatedWordDict(String word) throws IllegalArgumentException;
-}
