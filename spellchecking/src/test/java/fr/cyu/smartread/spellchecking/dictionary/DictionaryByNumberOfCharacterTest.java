2f1b1e82e6d482502a26a06f6b333822849e22e0
Thomas REMY <contact.t.remy777@gmail.com>
2023-02-19 21:10:41 +0100

    OCR-31 feat: add tests for dictionaries

--- spellchecking/src/test/java/fr/cyu/smartread/spellchecking/dictionary/DictionaryByNumberOfCharacterTest.java
+++ spellchecking/src/test/java/fr/cyu/smartread/spellchecking/dictionary/DictionaryByNumberOfCharacterTest.java
@@ -9,20 +9,67 @@ import static org.junit.jupiter.api.Assertions.*;
 class DictionaryByNumberOfCharacterTest {
     DictionaryByNumberOfCharacter dict;
 
-    @Test
-    void testInstantiateCorrectWordLength() {
-        assertThrowsExactly(IllegalArgumentException.class, () -> {
-            new DictionaryByNumberOfCharacter((short) 0);
-        });
-
+    @BeforeEach
+    public void setUp() {
         dict = new DictionaryByNumberOfCharacter((short) 5);
+    }
+
+    @Test
+    public void testInstantiateWithCorrectWordLength() {
         assertEquals(5, dict.getWordsLength());
 
         dict = new DictionaryByNumberOfCharacter((short) 25);
         assertEquals(25, dict.getWordsLength());
+    }
+
+    @Test
+    public void testInstantiateWithIncorrectWordLength() {
+        assertThrowsExactly(IllegalArgumentException.class, () -> {
+            new DictionaryByNumberOfCharacter((short) 0);
+        });
 
         assertThrowsExactly(IllegalArgumentException.class, () -> {
             new DictionaryByNumberOfCharacter((short) 26);
         });
+    }
+
+    @Test
+    public void testAddCorrectWord() {
+        assertDoesNotThrow(() -> {
+            dict.addWord("aboie");
+        });
+
+        assertDoesNotThrow(() -> {
+            dict.addWord("aider");
+        });
+    }
+
+    @Test
+    public void testAddIncorrectWord() {
+        assertThrowsExactly(WordNotSupportedException.class, () -> {
+            dict.addWord("abc");
+        });
+
+        assertThrowsExactly(WordNotSupportedException.class, () -> {
+            dict.addWord("abaissa");
+        });
+    }
+
+    @Test
+    public void testPopulateWithCorrectFile() {
+        assertDoesNotThrow(() -> {
+            dict.populateFromFile("src/test/resources/5-chars-dictionary-test.csv");
+        });
+
+        assertEquals(15, dict.getWordList().size());
+    }
+
+    @Test
+    public void testPopulateWithIncorrectFile() {
+        assertThrowsExactly(WordNotSupportedException.class, () -> {
+            dict.populateFromFile("src/test/resources/6-chars-dictionary-test.csv");
+        });
+
+        assertEquals(0, dict.getWordList().size());
     }
 }
--- spellchecking/src/test/java/fr/cyu/smartread/spellchecking/dictionary/DictionaryTest.java
+++ spellchecking/src/test/java/fr/cyu/smartread/spellchecking/dictionary/DictionaryTest.java
@@ -26,16 +26,32 @@ class DictionaryTest {
     }
 
     @Test
-    void testAddNewStringToList() {
+    void testAdd1NewWordsToDict() {
         dict.addWord("test");
         assertEquals(1, dict.getWordList().size());
     }
 
     @Test
-    void testPopulateFromFile() {
+    void testAdd3NewWordsToDict() {
+        dict.addWord("bonjour");
+        dict.addWord("au revoir");
+        dict.addWord("salut");
+        assertEquals(3, dict.getWordList().size());
+    }
+
+    @Test
+    void testPopulateFromCsvFile() {
         assertDoesNotThrow(() -> {
             dict.populateFromFile("src/test/resources/test-dictionary.csv");
         });
         assertEquals(3, dict.getWordList().size());
+    }
+
+    @Test
+    void testPopulateFromTxtFile() {
+        assertDoesNotThrow(() -> {
+            dict.populateFromFile("src/test/resources/test-dictionary.txt");
+        });
+        assertEquals(6, dict.getWordList().size());
     }
 }
