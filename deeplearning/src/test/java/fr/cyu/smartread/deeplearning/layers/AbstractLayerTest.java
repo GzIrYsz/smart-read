9dd8d89a1fb0d018b40674b34271c2e61e8ec017
Alexandre Rangom <alexandre.rangom@gmail.com>
2023-03-31 19:18:21 +0200

    feat(deeplearning/layer): Ajout de la classe abstraite pour les layers et des tests associés.

---
+++ deeplearning/src/test/java/fr/cyu/smartread/deeplearning/layers/AbstractLayerTest.java
@@ -0,0 +1,39 @@
+package fr.cyu.smartread.deeplearning.layers;
+
+import fr.cyu.smartread.deeplearning.activation.NoTrainingComputationsPerformedException;
+import org.ejml.EjmlUnitTests;
+import org.ejml.data.DMatrixRMaj;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbstractLayerTest {
+
+    private AbstractLayer layer;
+
+    @BeforeEach
+    public void setUp() {
+        layer = new Dropout(0.2F);
+    }
+
+    @Test
+    void shouldGetAnAssertionIfNoTrainingComputationPerformed() {
+        NoTrainingComputationsPerformedException thrown = Assertions.assertThrows(NoTrainingComputationsPerformedException.class, () -> {
+            layer.get_DZ_DParams_derivative();
+        });
+
+        assertEquals("You have not performed any training calculation, please use method trainingComputation before using this function", thrown.getMessage());
+    }
+
+    @Test
+    void shouldUpdateLastActionIfTrainingComputationPerformed() {
+        DMatrixRMaj matrix = new DMatrixRMaj(new double[][] {
+                {5, 5, 5}
+        });
+
+        DMatrixRMaj result = layer.trainingCompute(matrix);
+        EjmlUnitTests.assertEquals(result, matrix);
+    }
+}
