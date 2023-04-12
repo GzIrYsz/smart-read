41175e956b3859fcf5c3b2bd5b11ddefcbf477b4
Thomas REMY <contact.t.remy777@gmail.com>
2023-04-12 09:32:03 +0200

    refactor: add blank space

--- deeplearning/src/main/java/fr/cyu/smartread/deeplearning/losses/IncompatibleShapeException.java
+++ deeplearning/src/main/java/fr/cyu/smartread/deeplearning/losses/IncompatibleShapeException.java
@@ -2,7 +2,7 @@ package fr.cyu.smartread.deeplearning.losses;
 
 import org.ejml.data.DMatrixRMaj;
 
-public class IncompatibleShapeException extends Exception{
+public class IncompatibleShapeException extends Exception {
     public IncompatibleShapeException(DMatrixRMaj A, DMatrixRMaj B) {
         super(String.format("The dimensions of matrices A with (%d, %d) shape and B with (%d, %d) shape are not compatible for the calculation you are performing", A.getNumRows(), A.getNumCols(), B.getNumRows(), B.getNumCols()));
     }
