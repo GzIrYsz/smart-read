3dfa5ab56eaea2bb6f63019e17d01982f56c2ce6
Alexandre Rangom <alexandre.rangom@gmail.com>
2023-04-19 19:16:29 +0200

    fix (Dense): fix de bug
     Conflicts:
    	deeplearning/src/main/java/fr/cyu/smartread/deeplearning/layers/Dense.java

--- deeplearning/src/main/java/fr/cyu/smartread/deeplearning/layers/Dense.java
+++ deeplearning/src/main/java/fr/cyu/smartread/deeplearning/layers/Dense.java
@@ -2,8 +2,8 @@ package fr.cyu.smartread.deeplearning.layers;
 
 import fr.cyu.smartread.deeplearning.activations.AbstractActivation;
 import fr.cyu.smartread.deeplearning.activations.Relu;
-import fr.cyu.smartread.deeplearning.initializers.InitializerInterface;
 import fr.cyu.smartread.deeplearning.initializers.GlorotUniform;
+import fr.cyu.smartread.deeplearning.initializers.InitializerInterface;
 import fr.cyu.smartread.deeplearning.initializers.Zeros;
 import org.ejml.data.DMatrixRMaj;
 import org.ejml.dense.row.CommonOps_DDRM;
@@ -13,38 +13,38 @@ import java.util.ArrayList;
 
 public class Dense extends AbstractLayer {
     private boolean isInit = false;
-    private int shape;
-    private AbstractActivation activation;
-    private InitializerInterface weightsInitializer;
-    private InitializerInterface biasInitializer;
+    private final int nbNeurons;
+    private final AbstractActivation activation;
+    private final InitializerInterface weightsInitializer;
+    private final InitializerInterface biasInitializer;
     private DMatrixRMaj bias;
     private DMatrixRMaj weights;
-    private SimpleOperations_DDRM simpleOps = new SimpleOperations_DDRM();
+    private final SimpleOperations_DDRM simpleOps = new SimpleOperations_DDRM();
 
-    public Dense(int shape) {
-        this(shape, new Relu());
+    public Dense(int nbNeurons) {
+        this(nbNeurons, new Relu());
     }
 
-    public Dense(int shape, AbstractActivation activation) {
-        this(shape, activation, new GlorotUniform());
+    public Dense(int nbNeurons, AbstractActivation activation) {
+        this(nbNeurons, activation, new GlorotUniform());
     }
 
-    public Dense(int shape, InitializerInterface weightsInitializer) {
-        this(shape, new Relu(), weightsInitializer);
+    public Dense(int nbNeurons, InitializerInterface weightsInitializer) {
+        this(nbNeurons, new Relu(), weightsInitializer);
     }
 
-    public Dense(int shape, AbstractActivation activation, InitializerInterface weightsInitializer) {
-        this.shape = shape;
+    public Dense(int nbNeurons, AbstractActivation activation, InitializerInterface weightsInitializer) {
+        this.nbNeurons = nbNeurons;
         this.activation = activation;
         this.weightsInitializer = weightsInitializer;
         this.biasInitializer = new Zeros();
     }
 
     private void initLayer(DMatrixRMaj matrix) {
-        this.bias = new DMatrixRMaj(new double[this.shape][1]);
-        this.weights = new DMatrixRMaj(new double[matrix.getNumCols()][this.shape]);
-        this.biasInitializer.init(this.bias);
-        this.weightsInitializer.init(this.weights);
+        bias = new DMatrixRMaj(new double[this.nbNeurons][1]);
+        weights = new DMatrixRMaj(new double[matrix.getNumCols()][this.nbNeurons]);
+        biasInitializer.init(this.bias);
+        weightsInitializer.init(this.weights);
         isInit = true;
     }
 
@@ -57,19 +57,21 @@ public class Dense extends AbstractLayer {
         int startRow = 0;
         DMatrixRMaj out = CommonOps_DDRM.mult(Z, this.weights, null);
         CommonOps_DDRM.transpose(out);
+
         for (int i = 0; i < out.getNumCols(); i++) {
             DMatrixRMaj currentCol = CommonOps_DDRM.extractColumn(out, i, null);
-            currentCol = CommonOps_DDRM.add(currentCol, this.bias, null);
-            this.simpleOps.setColumn(out, i, startRow, currentCol.getData());
+            currentCol = CommonOps_DDRM.add(currentCol, bias, null);
+            simpleOps.setColumn(out, i, startRow, currentCol.getData());
         }
+
         CommonOps_DDRM.transpose(out);
-        return this.activation.compute(out);
+        return activation.compute(out);
     }
 
     @Override
     public ArrayList<DMatrixRMaj> compute_DZ_DParams_derivative() {
         ArrayList<DMatrixRMaj> derivatives = new ArrayList<>(2);
-        DMatrixRMaj biasDerivative = new DMatrixRMaj(new double[this.shape][1]);
+        DMatrixRMaj biasDerivative = new DMatrixRMaj(new double[nbNeurons][1]);
         biasDerivative.fill(1.0);
         
         derivatives.add(this.getLastFeed());
